17530e386141122c46cba2bc0cf7f3bee0f2be04
Tom <gh286991@gmail.com>
2021-10-02 01:48:23 +0800

    phone input format

--- App.tsx
+++ App.tsx
@@ -1,21 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [phone, setPhone] = useState<string>('');
+
+  const normalizeInput = (value: string) => {
+    if (!value) return setPhone(value);
+    const currentValue = value.replace(/[^\d]/g, '');
+    const cvLength = currentValue.length;
+
+    if (!phone || value.length > phone.length) {
+      if (cvLength < 4) return setPhone(currentValue);
+      if (cvLength < 7) return setPhone(`${currentValue.slice(0, 4)}-${currentValue.slice(4)}`);
+      return setPhone(
+        `${currentValue.slice(0, 4)}-${currentValue.slice(4, 7)}-${currentValue.slice(7, 10)}`,
+      );
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <Text>請輸入正確的電話號碼:</Text>
+        <TextInput
+          style={styles.input}
+          maxLength={12}
+          keyboardType={'number-pad'}
+          onChangeText={(value) => {
+            normalizeInput(value);
+          }}
+        >
+          {phone}
+        </TextInput>
+        <Text>你輸入的號碼:{phone}</Text>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    backgroundColor: 'red',
+  },
+  input: {
+    width: '60%',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
