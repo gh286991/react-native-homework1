b74e530ae79b0168b910038465a8f9146a413b29
Tom <gh286991@gmail.com>
2021-10-04 15:24:38 +0800

    input

--- App.tsx
+++ App.tsx
@@ -1,9 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, SafeAreaView } from 'react-native';
+import React, { useState, useRef, useCallback } from 'react';
+import { Animated, StyleSheet, Text, TextInput, SafeAreaView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   const [phone, setPhone] = useState<string>('');
+  const [isInitial, setIsInitial] = useState<boolean>(true);
+  const [right, setRight] = useState<boolean>(true);
+  const anim = useRef(new Animated.Value(0));
+  const rightAnswer = '0919-123-456';
+
+  const shake = useCallback(() => {
+    // makes the sequence loop
+    Animated.loop(
+      // runs the animation array in sequence
+      Animated.sequence([
+        Animated.timing(anim.current, {
+          toValue: -5,
+          duration: 50,
+          useNativeDriver: true,
+        }),
+
+        Animated.timing(anim.current, {
+          toValue: 5,
+          duration: 50,
+          useNativeDriver: true,
+        }),
+
+        Animated.timing(anim.current, {
+          toValue: 0,
+          duration: 50,
+          useNativeDriver: true,
+        }),
+      ]),
+      // loops the above animation config 2 times
+      { iterations: 2 },
+    ).start();
+  }, []);
 
   const normalizeInput = (value: string) => {
     if (!value) return setPhone(value);
@@ -19,23 +53,49 @@ export default function App() {
     }
   };
 
+  const onClickAnswer = () => {
+    if (phone === rightAnswer) {
+      setRight(true);
+      setIsInitial(false);
+    } else {
+      shake();
+      setRight(false);
+      setIsInitial(false);
+    }
+  };
+
   return (
     <>
       <StatusBar style="auto" />
-      <SafeAreaView style={styles.container}>
-        <Text>請輸入正確的電話號碼:</Text>
-        <TextInput
-          style={styles.input}
-          maxLength={12}
-          keyboardType={'number-pad'}
-          onChangeText={(value) => {
-            normalizeInput(value);
-          }}
-        >
-          {phone}
-        </TextInput>
-        <Text>你輸入的號碼:{phone}</Text>
-      </SafeAreaView>
+      <LinearGradient colors={['#a1c4fd', '#c2e9fb']} style={styles.linearGradient}>
+        <SafeAreaView style={styles.container}>
+          <Text>請輸入正確的電話號碼: </Text>
+
+          <Animated.View
+            style={{
+              transform: [{ translateX: anim.current }],
+              ...(right ? styles.input : styles.wrongInput),
+            }}
+          >
+            <TextInput
+              maxLength={12}
+              keyboardType={'number-pad'}
+              onChangeText={(value) => {
+                normalizeInput(value);
+              }}
+            >
+              {phone}
+            </TextInput>
+          </Animated.View>
+          <Text>你輸入的號碼:{phone}</Text>
+          {right && !isInitial ? (
+            <Text style={{ color: 'red', fontSize: 20 }}>輸入正確</Text>
+          ) : null}
+          <Button onPress={onClickAnswer} style={styles.button}>
+            <Text style={{ color: 'white', fontSize: 18 }}>確定</Text>
+          </Button>
+        </SafeAreaView>
+      </LinearGradient>
     </>
   );
 }
@@ -60,5 +120,23 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  wrongInput: {
+    width: '60%',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'red',
+  },
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: '#66B3FF',
+    margin: 20,
   },
 });
